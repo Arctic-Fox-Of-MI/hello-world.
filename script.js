603b6170fc8a530e8858f98b4afd8938c53a31b8
Brayden Bufton <249774859+Arctic-Fox-Of-MI@users.noreply.github.com>
2026-01-30 15:59:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.createElement('h1');
+    heading.textContent = '';
+    document.body.appendChild(heading);
+});
